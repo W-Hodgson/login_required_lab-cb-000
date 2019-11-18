84dbdd9da29abbb473e01ff2defe33061afcb4c0
Will Hodgson <will.hodgson@current-rms.com>
2019-11-18 05:20:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,8 @@ class ApplicationController < ActionController::Base
 
   def hello
     if !session[:name].exists?
-
+      redirect_to controller: 'sessions', action: 'new'
     end
-     redirect_to controller: 'sessions', action: 'new' unless session[:name]
    end
 
    def current_user
