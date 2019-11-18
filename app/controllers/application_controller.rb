7de77590bb985abbf365bbfe0e3ef6c88f38135d
Will Hodgson <will.hodgson@current-rms.com>
2019-11-18 05:20:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
+    if !session[:name].exists?
 
-  end
+    end
+     redirect_to controller: 'sessions', action: 'new' unless session[:name]
+   end
+
+   def current_user
+     session[:name]
+   end
+
+   private
+
+   def require_logged_in
+     redirect_to controller: 'sessions', action: 'new' unless current_user
+   end
 end
